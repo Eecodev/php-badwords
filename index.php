10d9606ee98f3867cdfbfa5f43446fa2f3b7df1b
Eecodev <mnasser.bbk@gmail.com>
2023-11-27 16:35:17 +0100

    scaffolding

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
-echo "ciao";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="execute.php" method="post">
+        <label for="paragraph">Write Paragraph:</label><br>
+        <textarea id="paragraph" name="paragraph" rows="4" cols="50"></textarea><br><br>
+        
+        <label for="word">Censured word:</label><br>
+        <input type="text" id="word" name="word"><br><br>
+        
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
